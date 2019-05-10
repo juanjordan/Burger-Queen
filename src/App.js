5c8adc648b13593b8131ba1f7bdc288488241c2b
Juan La Jara <wxomb@99.200-57-1.clientesbestel.com.mx>
2019-05-10 13:13:43 -0500

    Workshop 10 Mayo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button';
+import { Description, WelcomeTitle } from './components/Texts';
+
+const buttons = [1,2,3,4,5];
+
+const navigateToGoogle = () => {
+  window.location.href = "https://google.com";
+};
+
+const showModal = () => {
+  console.log('mostrar modal');
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <WelcomeTitle />
+        <Description />
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,6 +29,11 @@ function App() {
           Learn React
         </a>
       </header>
+      {/*buttons.map((button, index) =>
+        <Button name={`GDL ${index}`} color={`background-${index}`} key={index} />)*/
+      }
+      <Button name="Mostrar Modal" action={showModal} extraClassName="shadow" />
+      <Button name="Navega a Google" action={navigateToGoogle} />
     </div>
   );
 }
---
+++ src/components/Texts.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const Description = () => {
+  return (
+    <p>
+      Edit <code>src/App.js</code> and save to reload.
+    </p>
+  );
+};
+
+export const WelcomeTitle = () => {
+  return (
+    <h1>Bievenidxs</h1>
+  );
+};
